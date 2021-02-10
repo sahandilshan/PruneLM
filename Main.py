3a91548494efc383b27b4adab24638a76ee0e16d
Sahan Dilshan <32576163+Amstrom@users.noreply.github.com>
2021-02-10 22:58:18 +0530

    Fix Runtime issue

--- Main.py
+++ Main.py
@@ -36,7 +36,7 @@ for epoch in range(1, EPOCHS + 1):
     epoch_start_time = time.time()
     train(model, criterion, optimizer, corpus, TRAIN_DATA, epoch, EPOCHS,
           batch_size=BATCH_SIZE, sequence_length=SEQUENCE_LENGTH)
-    val_loss = evaluate(VAL_DATA, model, criterion, corpus, BATCH_SIZE, SEQUENCE_LENGTH)
+    val_loss = evaluate(VAL_DATA, model, criterion, NUM_TOKENS, BATCH_SIZE, SEQUENCE_LENGTH)
     print('-' * 89)
     print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
           'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
