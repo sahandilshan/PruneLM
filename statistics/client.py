8c76d28243bef9df535c047291466fa4f05b4487
Sahan Dilshan <32576163+Amstrom@users.noreply.github.com>
2021-02-14 22:50:49 +0530

    Add methods to set modal parameters and size

--- statistics/client.py
+++ statistics/client.py
@@ -7,20 +7,36 @@ class MyHttpClient:
     def __init__(self, url):
         self.url = url
 
-    def send_test_result(self, model_type, ppl):
+    def send_test_result(self, model_name, ppl):
         data = {
-            "model_type": f"{model_type}",
+            "model_name": f"{model_name}",
             "ppl": ppl
         }
         data = json.dumps(data)
         requests.post(url=self.url + '/test_result', json=data)
 
-    def send_valid_result(self, model_type, ppl):
+    def send_valid_result(self, model_name, ppl):
         data = {
-            "model_type": f"{model_type}",
+            "model_name": f"{model_name}",
             "ppl": ppl
         }
         data = json.dumps(data)
         requests.post(url=self.url + '/valid_result', json=data)
+
+    def send_model_size(self,  model_name, size):
+        data = {
+            "model_name": f"{model_name}",
+            "size": size
+        }
+        data = json.dumps(data)
+        requests.post(url=self.url + '/model_size', json=data)
+
+    def send_model_params(self,  model_name, params):
+        data = {
+            "model_name": f"{model_name}",
+            "params": params
+        }
+        data = json.dumps(data)
+        requests.post(url=self.url + '/model_params', json=data)
 
 
