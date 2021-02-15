cfcfa4b2965f6413d15ac6a99e2ad8fae666bb2d
Sahan Dilshan <32576163+Amstrom@users.noreply.github.com>
2021-02-15 18:59:01 +0530

    published statistics

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import torch
 from data.dataset import get_dataset
 from model.bi_lstm_lm import Bi_LSTM_Model, get_criterion, get_optimizer
 from prune.prune import Prune
+from statistics.server import start_prometheus_client
 from train.train import train
 from train.utils import evaluate
 from utils.parameters import get_total_parameters_count, get_pruned_parameters_count
@@ -44,7 +45,10 @@ print(f'Prune Type: {PRUNING_TYPE}, Percentage(s): {PERCENTAGES}')
 STAT_ENABLED = stat_configs['enable']
 STAT_ENABLED = True if STAT_ENABLED == 'true' else False
 SERVER_URL = stat_configs['server_url']
-client = MyHttpClient(SERVER_URL)
+PROMETHEUS_PORT = stat_configs['prometheus_port']
+client = None
+if STAT_ENABLED:
+    client = MyHttpClient(SERVER_URL)
 
 # Loading the model
 DEVICE = model_load_configs['device']
@@ -85,8 +89,9 @@ total_params = get_total_parameters_count(model)
 original_model_size = get_original_model_size(model)
 
 if STAT_ENABLED:
-    client.send_test_result('original', math.exp(test_loss))
-    client.send_valid_result('original', math.exp(val_loss))
+    start_prometheus_client(PROMETHEUS_PORT)
+    client.send_test_ppl('original', math.exp(test_loss))
+    client.send_valid_ppl('original', math.exp(val_loss))
     client.send_model_size('original', original_model_size)
     client.send_model_params('original', total_params)
 
@@ -101,12 +106,12 @@ if PRUNING_TYPE == 'basic' and PRUNING_ENABLED == 'true':
         prunedModel.load_state_dict(pruned_state_dic)
         pruned_model_params = get_pruned_parameters_count(prunedModel)
         show_parameters_stats(total_params, pruned_model_params)
-        model_name = 'pruned_model_' + str(percentage)
-        path = MODEL_SAVING_PATH + '/pruned_model_' + str(percentage) + '.ckpt'
+        model_name = 'pruned_model_' + str(percentage) + '.ckpt'
+        path = MODEL_SAVING_PATH + '/' + model_name
         torch.save(prunedModel.state_dict(), path)
         print('model saved.')
         print(f'Model saved path: {path}')
-        pruned_model_size = get_pruned_model_size(prunedModel)
+        # pruned_model_size = get_pruned_model_size(prunedModel)
         val_loss = evaluate(VALID_SET, prunedModel, criterion, NUM_TOKENS,
                             BATCH_SIZE, SEQUENCE_LENGTH)
         test_loss = evaluate(TEST_SET, prunedModel, criterion, NUM_TOKENS,
@@ -117,6 +122,10 @@ if PRUNING_TYPE == 'basic' and PRUNING_ENABLED == 'true':
         print('| test loss {:5.2f} | '
               'test ppl {:8.2f}'.format(test_loss, math.exp(test_loss)))
         print('-' * 89)
+        if STAT_ENABLED:
+            client.init_pruned_model(model_name)
+            client.send_valid_ppl(model_name, math.exp(val_loss))
+            client.send_test_ppl(model_name, math.exp(test_loss))
 
 # Iterative Pruning
 elif PRUNING_TYPE == 'iterative' and PRUNING_ENABLED == 'true':
@@ -132,19 +141,28 @@ elif PRUNING_TYPE == 'iterative' and PRUNING_ENABLED == 'true':
         prune = Prune(model, percentage)
         pruned_state_dic = prune.modelPruning()
         prunedModel.load_state_dict(pruned_state_dic)
-        path = MODEL_SAVING_PATH + '/pruned_model_' + str(percentage) + '.ckpt'
+        model_name = 'pruned_model_' + str(percentage) + '.ckpt'
+        path = MODEL_SAVING_PATH + '/' + model_name
+        if STAT_ENABLED:
+            client.init_pruned_model(model_name)
+            client.send_total_epoch_size(model_name, EPOCHS)
         for epoch in range(1, EPOCHS + 1):
+            if STAT_ENABLED:
+                client.send_current_epoch_number(model_name, epoch)
             epoch_start_time = time.time()
             train(prunedModel, prune_criterion, prune_optimizer, NUM_TOKENS, TRAIN_SET, epoch, EPOCHS,
-                  batch_size=BATCH_SIZE, sequence_length=SEQUENCE_LENGTH)
+                  batch_size=BATCH_SIZE, sequence_length=SEQUENCE_LENGTH,
+                  client=client, model_name=model_name)
             prune = Prune(prunedModel, percentage)
             pruned_state_dic = prune.modelPruning()
             prunedModel.load_state_dict(pruned_state_dic)
             val_loss = evaluate(VALID_SET, prunedModel, prune_criterion, NUM_TOKENS,
                                 BATCH_SIZE, SEQUENCE_LENGTH)
+            epoch_end_time = time.time()
+            elapsed_time = epoch_end_time - epoch_start_time
             print('-' * 70)
             print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-                  'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                  'valid ppl {:8.2f}'.format(epoch, elapsed_time,
                                              val_loss, math.exp(val_loss)))
             print('-' * 70)
             if MODEL_SAVING_TYPE == 'best':
@@ -154,6 +172,12 @@ elif PRUNING_TYPE == 'iterative' and PRUNING_ENABLED == 'true':
                     best_val_loss = val_loss
                 else:
                     print('Model not saving....')
+
+            if STAT_ENABLED:
+                client.send_valid_loss(model_name, val_loss)
+                client.send_valid_ppl(model_name, math.exp(val_loss))
+                client.send_last_epoch_elapsed_time(model_name, elapsed_time)
+                client.send_last_epoch_finished_time(model_name, epoch_end_time)
 
         if MODEL_SAVING_TYPE == 'last':
             torch.save(prunedModel.state_dict(), path)
@@ -177,11 +201,9 @@ for file in os.listdir(MODEL_SAVING_PATH):
         show_model_size_stats(original_model_size, pruned_model_size)
         print('-' * 100 + '\n')
         if STAT_ENABLED:
-            client.send_test_result(file, math.exp(test_loss))
-            client.send_valid_result(file, math.exp(val_loss))
+            client.send_test_ppl(file, math.exp(test_loss))
+            client.send_valid_ppl(file, math.exp(val_loss))
             client.send_model_size(file, pruned_model_size)
             client.send_model_params(file, pruned_model_params)
 
 print('done')
-
-# TODO get prometheus server url from the configs and init prometheus client
