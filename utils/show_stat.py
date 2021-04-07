ff37b95fc841fe35544d8165fca13b746f66cf3a
Sahan Dilshan <32576163+Amstrom@users.noreply.github.com>
2021-04-07 15:04:00 +0530

    Improve the statistics logs

--- utils/show_stat.py
+++ utils/show_stat.py
@@ -8,9 +8,9 @@ def show_parameters_stats(total_params, pruned_model_params):
 
 
 def show_model_size_stats(original_model_size, pruned_model_size):
-    print(f'Original Model size: {original_model_size}, '
-          f'Pruned Model size: {pruned_model_size}')
+    print(f'Original Model size: {original_model_size}MB, '
+          f'Pruned Model size: {pruned_model_size}MB')
     compressed_size = round(original_model_size - pruned_model_size, 2)
-    print(f'Reduced size:{compressed_size}')
+    print(f'Reduced size:{compressed_size}MB')
     print(f'Compressed Percentage'
-          f': {round(compressed_size / original_model_size * 100, 2)}')
+          f': {round(compressed_size / original_model_size * 100, 2)}%')
